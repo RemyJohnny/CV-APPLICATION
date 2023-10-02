56cce73c661aca504ab4cba1180987b9fa8f8da5
Remy Johnny <ewerejohnnyjnr93@gmail.com>
2023-10-03 00:04:24 +0300

    updated the example Data

--- src/example.js
+++ src/example.js
@@ -2,20 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 export const example = {
   fullName: "Remy Johnny",
   email: "example123@gmail.com",
-  tel: "08072905382",
-  address: " Minsk  BY",
+  tel: "+375 25 695 58 19",
+  address: " Minsk, Belarus",
   education: [
     {
       key: uuidv4(),
       school: "Miva University",
-      degree: "Bachelor in Cybersecurity",
+      degree: "Bachelor's degree in Computer Science",
       startDate: "06/11/2018",
       endDate: "14/10/2023",
     },
     {
       key: uuidv4(),
-      school: "Delta State University",
-      degree: "masters in Computer Science",
+      school: "belarusian State University",
+      degree: "masters in IoT",
       startDate: "06/11/2023",
       endDate: "present",
     },
@@ -23,19 +23,19 @@ export const example = {
   experience: [
     {
       key: uuidv4(),
-      companyName: "RemyAccess inc",
-      position: "Chief technology Officer (CTO)",
-      startDateEX: "01/09/2023",
+      companyName: "RemyAccess Tech inc",
+      position: "React frontEnd Developer",
+      startDateEX: "23/05/2023",
       endDateEX: "present",
-      desc: "Designed and prototyped user interface patterns for various clients in various industries",
+      desc: "working with React framework and tailwind CSS to design and prototype user interface patterns for various clients in various industries",
     },
     {
       key: uuidv4(),
       companyName: "Apple inc",
-      position: "sales Manager ",
+      position: "software Engineer intern",
       startDateEX: "01/09/2023",
       endDateEX: "present",
-      desc: "sold 5000 iphone 15 pro max in 2 hours",
+      desc: "working with product team to plan ant set timelines for future projects,and also test completed programs and application against client requirements",
     },
   ],
 };
